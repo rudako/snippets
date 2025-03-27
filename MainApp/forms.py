fec34295fe883b5c975fbc190ebfb3c83b5304db
Vladimir Rudakov <rudakoff@gmail.com>
2025-03-27 14:19:56 +0300

    Create new form

--- MainApp/forms.py
+++ MainApp/forms.py
@@ -1,9 +1,20 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, ValidationError, TextInput, Textarea
 from MainApp.models import Snippet
 
 
 class SnippetForm(ModelForm):
-   class Meta:
+    class Meta:
        model = Snippet
        # Описываем поля, которые будем заполнять в форме
        fields = ['name', 'lang', 'code']
+       labels = {'name': '', 'lang': '', 'code': ''}
+       widgets = {
+           'name': TextInput(attrs={'placeholder': 'Название сниппета'}),
+           'code': Textarea(attrs={'placeholder': 'Код сниппета'})
+       }
+
+    def clean_name(self):
+       snippet_name = self.cleaned_data.get('name')
+       if len(snippet_name) > 3:
+           return snippet_name
+       raise ValidationError('Snippet name too short')
